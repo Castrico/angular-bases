1d5f302e0c9cd4a014a08e9c4bca6c36b9083129
Yeferson Castro <yefersoncastrogiraldo@gmail.com>
2024-05-14 20:39:47 -0500

    Fin de seccion 4

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,19 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CounterModule } from './counter/components/counter.module';
+import { HerosModule } from './heroes/heroes.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    CounterModule,
+    HerosModule
   ],
   providers: [],
   bootstrap: [AppComponent]
